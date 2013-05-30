ebe99269604a39296bb0431fcf221a4ab56c0f01
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-30 17:17:10 +0700

    Add http server and execute inline function

--- hello.js
+++ hello.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+var http = require("http");
+
+http.createServer(function(request,response){
+	response.writeHead(200,{"Content-Type": "text/plain"});
+	response.write("Hello");
+	response.end();
+	console.log("Receive a request");
+	execute(say,"hi");
+}).listen(8888);
+console.log("Listen at localhost:8888");
+
+
+function say(word){
+	console.log(word);
+}
+
+function execute(someFunction, value){
+	someFunction(value);
+}
